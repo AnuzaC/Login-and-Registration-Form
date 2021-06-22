142cbee1b8d6d26cf5d7882cf66064b807ab22fc
Anuja Chaudhary <anuza.ch051@gmail.com>
2021-06-22 17:06:11 +0545

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,6 +1,26 @@
+const login= document.querySelector("#login");
+
+login.onclick = function(){
+    let nameCheck=localStorage.getItem(userName);
+    let passCheck=localStorage.getItem(userPass);
+
+    let uname=document.querySelector('#uname');
+    let pass=document.querySelector('#pass');
+
+    document.querySelector('.err-pass').style.color ="red";
+
+    if (uname.value!=nameCheck.value || pass.value!=passCheck.value){
+        login.disabled=true;
+        document.querySelector('.err-pass').innerHTML="Username and password donot match";
+    }
+    else{
+        return true;
+    }
+};
+
 //Hide and Show Password
 const togglePassword = document.querySelector('#togglePassword');
-  const password = document.querySelector('#id_password');
+const password = document.querySelector('#password');
  
   togglePassword.addEventListener('click', function (e) {
     // toggle the type attribute
