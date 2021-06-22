59e3edb72646b3901bd0b92eedf467a5dbc7e468
Anuja Chaudhary <anuza.ch051@gmail.com>
2021-06-22 17:05:37 +0545

    Update signup.js

--- js/signup.js
+++ js/signup.js
@@ -6,19 +6,35 @@ register.onclick = function(){
     let email=document.querySelector('#email');
     let pass=document.querySelector('#pass');
     let passConfirm =document.querySelector('#pass-confirm');
+    let checkbox = document.querySelector('#checkbox')
+
     document.querySelector('.err-pass').style.color ="red";
+    document.querySelector('.err-pass-no-match').style.color ="red";
 
     // ----------------Password length Validation----------------------
     
-    if (pass.length<6){
+    if (pass.value.length<6){
         register.disabled=true;
         document.querySelector('.err-pass').innerHTML="Password must be at least 6 character long";
+        pass.focus();
     }
 
     else if(pass.value!=passConfirm.value){
         register.disabled=true;
         document.querySelector('.err-pass-no-match').innerHTML="Passwords do not match";
+        passConfirm.focus();
+    }
+
+    else if(checkbox.value===''){
+        register.disabled=true;
+        document.querySelector('.err-checkbox').innerHTML="Please select this field";
     }
 };
+
+let userName= localStorage.setItem(uname);
+let userEmail= localStorage.setItem(email);
+let userPass= localStorage.setItem(pass);
+
+
 
 
