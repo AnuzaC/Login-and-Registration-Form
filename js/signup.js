33216d3a3a1f0647b9a17d28e9b7bbe8ac6aebcf
Anuja Chaudhary <anuza.ch051@gmail.com>
2021-06-22 15:39:28 +0545

    Update signup.js

--- js/signup.js
+++ js/signup.js
@@ -1,4 +1,24 @@
-const btn = document.querySelector("#register");
-function formValidation(){
+const register = document.querySelector("#register");
+
+register.onclick = function(){
+
+    let uname=document.querySelector('#uname');
+    let email=document.querySelector('#email');
+    let pass=document.querySelector('#pass');
+    let passConfirm =document.querySelector('#pass-confirm');
+    document.querySelector('.err-pass').style.color ="red";
+
+    // ----------------Password length Validation----------------------
     
-}
+    if (pass.length<6){
+        register.disabled=true;
+        document.querySelector('.err-pass').innerHTML="Password must be at least 6 character long";
+    }
+
+    else if(pass.value!=passConfirm.value){
+        register.disabled=true;
+        document.querySelector('.err-pass-no-match').innerHTML="Passwords do not match";
+    }
+};
+
+
