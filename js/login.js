f6c06e03513405513d825bb9a9f851baa3ba8ede
Anuja Chaudhary <anuza.ch051@gmail.com>
2021-06-23 11:35:43 +0545

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,8 +1,8 @@
-const login= document.querySelector("#login");
+const login= document.querySelector(".login");
 
 login.onclick = function(){
-    let nameCheck=localStorage.getItem(uname);
-    let passCheck=localStorage.getItem(pass);
+    let nameCheck=localStorage.getItem("uname");
+    let passCheck=localStorage.getItem("pass");
 
     let username= document.querySelector('#uname');
     let userpw= document.querySelector('#pass');
@@ -10,22 +10,22 @@ login.onclick = function(){
 
     document.querySelector('.err-pass').style.color ="red";
 
-    if (nameCheck!=username.value || passCheck!=userpw.value){
+    if (nameCheck!=username.value && passCheck!=userpw.value){
         login.disabled=true;
         document.querySelector('.err-pass').innerHTML="Username and password donot match";
     }
 
     else{
         login.disabled=false;
-        window.location.href="../welcome.html";
-        //window.location.replace("../welcome.html");
+        //window.open("../welcome.html");
+        window.location.replace("../welcome.html");
     }
     return false;
 };
 
 //Hide and Show Password
 const togglePassword = document.querySelector('#togglePassword');
-const password = document.querySelector('#password');
+const password = document.querySelector('#pass');
  
   togglePassword.addEventListener('click', function (e) {
     // toggle the type attribute
