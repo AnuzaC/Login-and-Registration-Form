430f883f9e580704836b99728987d982963ea3f7
Anuja Chaudhary <anuza.ch051@gmail.com>
2021-06-24 10:12:01 +0545

    Update signup.js

--- js/signup.js
+++ js/signup.js
@@ -1,43 +1,3 @@
-const register = document.querySelector("#register");
-
-register.onclick = function(){
-
-    let uname=document.querySelector('#uname');
-    let email=document.querySelector('#email');
-    let pass=document.querySelector('#pass');
-    let passConfirm =document.querySelector('#pass-confirm');
-    let checkbox = document.querySelector('#checkbox');
-
-    document.querySelector('.err-pass').style.color ="red";
-    document.querySelector('.err-pass-no-match').style.color ="red";
-
-    // ----------------Password length Validation----------------------
-    
-    if (pass.value.length<6){
-        register.disabled=true;
-        document.querySelector('.err-pass').innerHTML="Password must be at least 6 character long";
-        pass.focus();
-    }
-
-    else if(pass.value!=passConfirm.value){
-        register.disabled=true;
-        document.querySelector('.err-pass-no-match').innerHTML="Passwords do not match";
-        passConfirm.focus();
-    }
-
-    else if(checkbox.value==''){
-        register.disabled=true;
-        document.querySelector('.err-checkbox').innerHTML="Please select this field";
-    }
-    else{
-        register.disabled=false;
-        localStorage.setItem("uname",uname.value);
-        localStorage.setItem("email",email.value);
-        localStorage.setItem("pass",pass.value);
-        window.location.replace("../login.html");
-    }
-    return false;
-};
 // ----------------Password Strength Display-----------------------
 let pass=document.querySelector('#pass');
 let passStrength = document.querySelector('#pass-strength');
@@ -83,3 +43,62 @@ pass.addEventListener("input",()=>{
     }
 
 });
+
+const register = document.querySelector("#register");
+
+register.onclick = function(){
+
+    let uname=document.querySelector('#uname');
+    let email=document.querySelector('#email');
+    let pass=document.querySelector('#pass');
+    let passConfirm =document.querySelector('#pass-confirm');
+    let checkbox = document.querySelector('#checkbox');
+
+    document.querySelector('.err-pass').style.color ="red";
+    document.querySelector('.err-pass-no-match').style.color ="red";
+
+
+    if(uname.value.length==0){
+        login.disabled=true;
+        document.querySelector('.err-name').innerHTML="Please enter username";
+        uname.focus();
+    }
+    else if(email.value.length==0){
+        login.disabled=true;
+        document.querySelector('.err-pass').innerHTML="Please enter email";
+        email.focus();
+    }
+    else if(pass.value.length==0){
+        login.disabled=true;
+        document.querySelector('.err-pass').innerHTML="Please enter password";
+        pass.focus();
+    }
+
+    // ----------------Password length Validation----------------------
+    
+    else if (pass.value.length<6){
+        register.disabled=true;
+        document.querySelector('.err-pass').innerHTML="Password must be at least 6 character long";
+        pass.focus();
+    }
+
+    else if(pass.value!=passConfirm.value){
+        register.disabled=true;
+        document.querySelector('.err-pass-no-match').innerHTML="Passwords do not match";
+        passConfirm.focus();
+    }
+
+    else if(checkbox.value==''){
+        register.disabled=true;
+        document.querySelector('.err-checkbox').innerHTML="Please select this field";
+    }
+    else if(uname.value.length!=0 && email.value.length!=0 && pass.value.length!=0 && pass.value.length>=6 && pass.value==passConfirm.value){
+        register.disabled=false;
+        localStorage.setItem("uname",uname.value);
+        localStorage.setItem("email",email.value);
+        localStorage.setItem("pass",pass.value);
+        window.location.replace("../login.html");
+        //window.open('../login.html');
+    }
+    return false;
+};
